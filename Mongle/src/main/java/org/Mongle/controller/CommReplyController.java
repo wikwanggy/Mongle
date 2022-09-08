e9b5df6b1a4227dfa58a8d887721f90a7accb207
GR <GR@DESKTOP-ODLO1F7>
2022-09-08 11:11:34 +0900

    민서

--- Mongle/src/main/java/org/Mongle/controller/CommReplyController.java
+++ Mongle/src/main/java/org/Mongle/controller/CommReplyController.java
@@ -18,20 +18,20 @@ public class CommReplyController {
 	@Autowired
 	CommReplyService crs;
 	
-	@RequestMapping(value="/replies/new", method=RequestMethod.POST)
-	public ResponseEntity<String> replywrite(@RequestBody CommReplyupVo crv){
+	@RequestMapping(value="/commreply/new", method=RequestMethod.POST)
+	public ResponseEntity<String> replycomwrt(@RequestBody CommReplyupVo crv){
 		int result=crs.rewrite(crv);
 		System.out.println(crv);
 		return result==1?new ResponseEntity<>("success",HttpStatus.OK)
 				:new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
 	}
-	@RequestMapping (value = "/replies/{bno}", method = RequestMethod.GET)
-	public ResponseEntity<ArrayList<CommReplyupVo>> replylist(@PathVariable int bno) { //비동기식으로 처리해야 할 경우 @PathVariable
+	@RequestMapping (value = "/commreplies/{bno}", method = RequestMethod.GET)
+	public ResponseEntity<ArrayList<CommReplyupVo>> commreplylist(@PathVariable int bno) { //비동기식으로 처리해야 할 경우 @PathVariable
 		System.out.println(bno);
 		return new ResponseEntity<>(crs.list(bno),HttpStatus.OK);
 	}
-	@RequestMapping (value = "/replies/remove/{rno}", method = RequestMethod.DELETE)
-	public ResponseEntity<String> replyremove(@PathVariable int rno){
+	@RequestMapping (value = "/commreplies/remove/{rno}", method = RequestMethod.DELETE)
+	public ResponseEntity<String> commreplyremove(@PathVariable int rno){
 		System.out.println(rno+"번 삭제");
 	    int result=crs.remove(rno);
 		return result==1?new ResponseEntity<>("success",HttpStatus.OK)
