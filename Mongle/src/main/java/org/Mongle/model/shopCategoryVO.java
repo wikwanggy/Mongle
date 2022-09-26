734a31efdfe8b066ae8627df37ff26673e7e2625
admin <admin@222.97.223.107>
2022-09-26 22:43:26 +0900

    상품 VO추가 1개

---
+++ Mongle/src/main/java/org/Mongle/model/shopCategoryVO.java
@@ -0,0 +1,23 @@
+package org.Mongle.model;
+
+public class shopCategoryVO {
+	private String c_type1;
+	private String c_type2;
+	public String getC_type1() {
+		return c_type1;
+	}
+	public void setC_type1(String c_type1) {
+		this.c_type1 = c_type1;
+	}
+	public String getC_type2() {
+		return c_type2;
+	}
+	public void setC_type2(String c_type2) {
+		this.c_type2 = c_type2;
+	}
+	@Override
+	public String toString() {
+		return "shopCategoryVO [c_type1=" + c_type1 + ", c_type2=" + c_type2 + "]";
+	}
+	
+}
