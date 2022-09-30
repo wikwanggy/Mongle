d0ebb43767165090d72cebfcf142cd21246d1945
GR <GR@DESKTOP-ODLO1F7>
2022-09-30 09:23:07 +0900

    주소,getid

--- Mongle/src/main/java/org/Mongle/Service/LoginServiceImpl.java
+++ Mongle/src/main/java/org/Mongle/Service/LoginServiceImpl.java
@@ -14,7 +14,6 @@ import org.apache.commons.mail.HtmlEmail;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-
 @Service
 public class LoginServiceImpl implements LoginService {
 	@Autowired
